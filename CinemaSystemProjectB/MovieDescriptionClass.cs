74b21aa5ede62b8dc41b0dd0362dcf065ee22142
WingmanChung <wingmanchung12@gmail.com>
2020-05-04 16:02:51 +0200

    Usercontrol Movieschedule
    Movieschedule layout aangepast en filmtijden toegevoegd
    Functies gemaakt om filmrondes te laten
    Filmbeschrijving laten zien wanneer je fillm selecteert in filmrooster

--- CinemaSystemProjectB/MovieDescriptionClass.cs
+++ CinemaSystemProjectB/MovieDescriptionClass.cs
@@ -15,6 +15,7 @@ namespace CinemaSystemProjectB
         public string Language { get; set; }
         public string Rating { get; set; }
         public string Price { get; set; }
+        public string Runtime { get; set; }
         public string FilmTechnology { get; set; }
         public string Trailer { get; set; }
         public string Synopsis { get; set; }
