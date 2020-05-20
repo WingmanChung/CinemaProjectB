471453688f0f9f2b91bdf89b989679a5b7f29202
_wingyeechung <0991394@hr.nl>
2020-05-20 03:07:37 +0200

    stappenplan reservering aangepast

--- CinemaSystemProjectB/RoadMapItem.Designer.cs
+++ CinemaSystemProjectB/RoadMapItem.Designer.cs
@@ -45,7 +45,7 @@
             this.RoadMapInfo.ForeColor = System.Drawing.Color.Yellow;
             this.RoadMapInfo.Location = new System.Drawing.Point(0, 0);
             this.RoadMapInfo.Name = "RoadMapInfo";
-            this.RoadMapInfo.Size = new System.Drawing.Size(1000, 659);
+            this.RoadMapInfo.Size = new System.Drawing.Size(1000, 722);
             this.RoadMapInfo.TabIndex = 1;
             this.RoadMapInfo.Text = resources.GetString("RoadMapInfo.Text");
             // 
@@ -65,7 +65,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Yellow;
-            this.label2.Location = new System.Drawing.Point(-1, 110);
+            this.label2.Location = new System.Drawing.Point(-1, 106);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(146, 19);
             this.label2.TabIndex = 67;
@@ -76,7 +76,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Yellow;
-            this.label4.Location = new System.Drawing.Point(0, 182);
+            this.label4.Location = new System.Drawing.Point(0, 180);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(106, 19);
             this.label4.TabIndex = 68;
@@ -87,7 +87,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Yellow;
-            this.label5.Location = new System.Drawing.Point(0, 308);
+            this.label5.Location = new System.Drawing.Point(-1, 323);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(177, 19);
             this.label5.TabIndex = 69;
@@ -98,7 +98,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Yellow;
-            this.label6.Location = new System.Drawing.Point(0, 362);
+            this.label6.Location = new System.Drawing.Point(-1, 431);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(219, 19);
             this.label6.TabIndex = 70;
@@ -109,7 +109,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Yellow;
-            this.label7.Location = new System.Drawing.Point(0, 433);
+            this.label7.Location = new System.Drawing.Point(-1, 558);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(116, 19);
             this.label7.TabIndex = 71;
@@ -120,7 +120,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.Yellow;
-            this.label8.Location = new System.Drawing.Point(0, 506);
+            this.label8.Location = new System.Drawing.Point(-1, 630);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(191, 19);
             this.label8.TabIndex = 72;
@@ -140,7 +140,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.RoadMapInfo);
             this.Name = "RoadMapItem";
-            this.Size = new System.Drawing.Size(1000, 659);
+            this.Size = new System.Drawing.Size(1000, 722);
             this.ResumeLayout(false);
             this.PerformLayout();
 
