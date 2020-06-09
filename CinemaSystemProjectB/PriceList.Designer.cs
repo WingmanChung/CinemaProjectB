f10484c1181f77aa5cb8d582c155c52657ed35eb
JollyRiB <JollyRigo171@hotmail.com>
2020-06-09 20:20:26 +0200

    Removed Maximize buttons

--- CinemaSystemProjectB/PriceList.Designer.cs
+++ CinemaSystemProjectB/PriceList.Designer.cs
@@ -398,6 +398,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gradientLabel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
             this.Name = "PriceList";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PriceList";
--- CinemaSystemProjectB/SeatReservation.Designer.cs
+++ CinemaSystemProjectB/SeatReservation.Designer.cs
@@ -61,9 +61,10 @@
             // 
             this.label16.AutoSize = true;
             this.label16.ForeColor = System.Drawing.Color.White;
-            this.label16.Location = new System.Drawing.Point(311, 687);
+            this.label16.Location = new System.Drawing.Point(415, 846);
+            this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(73, 13);
+            this.label16.Size = new System.Drawing.Size(96, 17);
             this.label16.TabIndex = 168;
             this.label16.Text = "Beste Stoelen";
             // 
@@ -71,9 +72,10 @@
             // 
             this.label15.AutoSize = true;
             this.label15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
-            this.label15.Location = new System.Drawing.Point(292, 687);
+            this.label15.Location = new System.Drawing.Point(389, 846);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(13, 13);
+            this.label15.Size = new System.Drawing.Size(16, 17);
             this.label15.TabIndex = 167;
             this.label15.Text = "  ";
             // 
@@ -81,9 +83,10 @@
             // 
             this.label14.AutoSize = true;
             this.label14.ForeColor = System.Drawing.Color.White;
-            this.label14.Location = new System.Drawing.Point(102, 715);
+            this.label14.Location = new System.Drawing.Point(136, 880);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(78, 13);
+            this.label14.Size = new System.Drawing.Size(103, 17);
             this.label14.TabIndex = 166;
             this.label14.Text = "Goede Stoelen";
             // 
@@ -91,9 +94,10 @@
             // 
             this.label13.AutoSize = true;
             this.label13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(228)))), ((int)(((byte)(0)))));
-            this.label13.Location = new System.Drawing.Point(83, 715);
+            this.label13.Location = new System.Drawing.Point(111, 880);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(13, 13);
+            this.label13.Size = new System.Drawing.Size(16, 17);
             this.label13.TabIndex = 165;
             this.label13.Text = "  ";
             // 
@@ -101,9 +105,10 @@
             // 
             this.label12.AutoSize = true;
             this.label12.ForeColor = System.Drawing.Color.White;
-            this.label12.Location = new System.Drawing.Point(102, 687);
+            this.label12.Location = new System.Drawing.Point(136, 846);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(85, 13);
+            this.label12.Size = new System.Drawing.Size(113, 17);
             this.label12.TabIndex = 164;
             this.label12.Text = "Normale Stoelen";
             // 
@@ -111,9 +116,10 @@
             // 
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(162)))), ((int)(((byte)(232)))));
-            this.label11.Location = new System.Drawing.Point(83, 687);
+            this.label11.Location = new System.Drawing.Point(111, 846);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.Size = new System.Drawing.Size(16, 17);
             this.label11.TabIndex = 163;
             this.label11.Text = "  ";
             // 
@@ -123,9 +129,10 @@
             this.SeatConfirmButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SeatConfirmButton.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SeatConfirmButton.ForeColor = System.Drawing.Color.Black;
-            this.SeatConfirmButton.Location = new System.Drawing.Point(909, 687);
+            this.SeatConfirmButton.Location = new System.Drawing.Point(1212, 846);
+            this.SeatConfirmButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SeatConfirmButton.Name = "SeatConfirmButton";
-            this.SeatConfirmButton.Size = new System.Drawing.Size(137, 41);
+            this.SeatConfirmButton.Size = new System.Drawing.Size(183, 50);
             this.SeatConfirmButton.TabIndex = 161;
             this.SeatConfirmButton.Text = "Bevestigen";
             this.SeatConfirmButton.UseVisualStyleBackColor = false;
@@ -137,17 +144,19 @@
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.Maroon;
-            this.label2.Location = new System.Drawing.Point(1080, 121);
+            this.label2.Location = new System.Drawing.Point(1440, 149);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 636);
+            this.label2.Size = new System.Drawing.Size(40, 783);
             this.label2.TabIndex = 148;
             // 
             // Linkerkolom
             // 
             this.Linkerkolom.BackColor = System.Drawing.Color.Maroon;
-            this.Linkerkolom.Location = new System.Drawing.Point(-2, 121);
+            this.Linkerkolom.Location = new System.Drawing.Point(-3, 149);
+            this.Linkerkolom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Linkerkolom.Name = "Linkerkolom";
-            this.Linkerkolom.Size = new System.Drawing.Size(30, 629);
+            this.Linkerkolom.Size = new System.Drawing.Size(40, 774);
             this.Linkerkolom.TabIndex = 147;
             // 
             // PriceList
@@ -156,9 +165,10 @@
             this.PriceList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PriceList.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.PriceList.ForeColor = System.Drawing.Color.Black;
-            this.PriceList.Location = new System.Drawing.Point(734, 639);
+            this.PriceList.Location = new System.Drawing.Point(979, 786);
+            this.PriceList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PriceList.Name = "PriceList";
-            this.PriceList.Size = new System.Drawing.Size(108, 30);
+            this.PriceList.Size = new System.Drawing.Size(144, 37);
             this.PriceList.TabIndex = 193;
             this.PriceList.Text = "Prijzenlijst";
             this.PriceList.UseVisualStyleBackColor = false;
@@ -168,9 +178,10 @@
             // 
             // SeatFlowPanel
             // 
-            this.SeatFlowPanel.Location = new System.Drawing.Point(62, 154);
+            this.SeatFlowPanel.Location = new System.Drawing.Point(83, 190);
+            this.SeatFlowPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SeatFlowPanel.Name = "SeatFlowPanel";
-            this.SeatFlowPanel.Size = new System.Drawing.Size(604, 486);
+            this.SeatFlowPanel.Size = new System.Drawing.Size(805, 598);
             this.SeatFlowPanel.TabIndex = 194;
             this.SeatFlowPanel.MouseEnter += new System.EventHandler(this.SeatReservation_MouseEnter);
             // 
@@ -179,18 +190,20 @@
             this.SelectedSeatsLabel.AutoSize = true;
             this.SelectedSeatsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SelectedSeatsLabel.ForeColor = System.Drawing.Color.Yellow;
-            this.SelectedSeatsLabel.Location = new System.Drawing.Point(3, 0);
+            this.SelectedSeatsLabel.Location = new System.Drawing.Point(4, 0);
+            this.SelectedSeatsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SelectedSeatsLabel.Name = "SelectedSeatsLabel";
-            this.SelectedSeatsLabel.Size = new System.Drawing.Size(0, 18);
+            this.SelectedSeatsLabel.Size = new System.Drawing.Size(0, 23);
             this.SelectedSeatsLabel.TabIndex = 196;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(753, 154);
+            this.label1.Location = new System.Drawing.Point(1004, 190);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 35);
+            this.label1.Size = new System.Drawing.Size(275, 43);
             this.label1.TabIndex = 197;
             this.label1.Text = "Geselecteerde stoelen";
             // 
@@ -199,9 +212,10 @@
             this.AdditionalCosts.AutoSize = true;
             this.AdditionalCosts.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.AdditionalCosts.ForeColor = System.Drawing.Color.Yellow;
-            this.AdditionalCosts.Location = new System.Drawing.Point(843, 645);
+            this.AdditionalCosts.Location = new System.Drawing.Point(1124, 794);
+            this.AdditionalCosts.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.AdditionalCosts.Name = "AdditionalCosts";
-            this.AdditionalCosts.Size = new System.Drawing.Size(138, 18);
+            this.AdditionalCosts.Size = new System.Drawing.Size(176, 23);
             this.AdditionalCosts.TabIndex = 198;
             this.AdditionalCosts.Text = "Extra kosten €0,00";
             // 
@@ -209,9 +223,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Lime;
-            this.label4.Location = new System.Drawing.Point(292, 715);
+            this.label4.Location = new System.Drawing.Point(389, 880);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.Size = new System.Drawing.Size(16, 17);
             this.label4.TabIndex = 199;
             this.label4.Text = "  ";
             // 
@@ -219,9 +234,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(311, 715);
+            this.label5.Location = new System.Drawing.Point(415, 880);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.Size = new System.Drawing.Size(153, 17);
             this.label5.TabIndex = 200;
             this.label5.Text = "Geselecteerde Stoelen";
             // 
@@ -229,9 +245,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Gray;
-            this.label6.Location = new System.Drawing.Point(494, 687);
+            this.label6.Location = new System.Drawing.Point(659, 846);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.Size = new System.Drawing.Size(16, 17);
             this.label6.TabIndex = 201;
             this.label6.Text = "  ";
             // 
@@ -239,18 +256,20 @@
             // 
             this.label7.AutoSize = true;
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(513, 687);
+            this.label7.Location = new System.Drawing.Point(684, 846);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.Size = new System.Drawing.Size(115, 17);
             this.label7.TabIndex = 202;
             this.label7.Text = "Niet Beschikbaar";
             // 
             // label8
             // 
             this.label8.BackColor = System.Drawing.Color.Maroon;
-            this.label8.Location = new System.Drawing.Point(734, 177);
+            this.label8.Location = new System.Drawing.Point(979, 218);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(246, 3);
+            this.label8.Size = new System.Drawing.Size(328, 4);
             this.label8.TabIndex = 203;
             // 
             // selectedSeatsPanel
@@ -258,26 +277,29 @@
             this.selectedSeatsPanel.AutoScroll = true;
             this.selectedSeatsPanel.Controls.Add(this.SelectedSeatsLabel);
             this.selectedSeatsPanel.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectedSeatsPanel.Location = new System.Drawing.Point(734, 201);
+            this.selectedSeatsPanel.Location = new System.Drawing.Point(979, 247);
+            this.selectedSeatsPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.selectedSeatsPanel.Name = "selectedSeatsPanel";
-            this.selectedSeatsPanel.Size = new System.Drawing.Size(246, 377);
+            this.selectedSeatsPanel.Size = new System.Drawing.Size(328, 464);
             this.selectedSeatsPanel.TabIndex = 204;
             // 
             // label9
             // 
             this.label9.BackColor = System.Drawing.Color.Maroon;
-            this.label9.Location = new System.Drawing.Point(734, 597);
+            this.label9.Location = new System.Drawing.Point(979, 735);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(246, 3);
+            this.label9.Size = new System.Drawing.Size(328, 4);
             this.label9.TabIndex = 205;
             // 
             // Screen
             // 
             this.Screen.BackColor = System.Drawing.Color.Gray;
             this.Screen.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Screen.Location = new System.Drawing.Point(82, 643);
+            this.Screen.Location = new System.Drawing.Point(109, 791);
+            this.Screen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Screen.Name = "Screen";
-            this.Screen.Size = new System.Drawing.Size(563, 23);
+            this.Screen.Size = new System.Drawing.Size(751, 28);
             this.Screen.TabIndex = 206;
             this.Screen.Text = "Scherm";
             this.Screen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -289,17 +311,18 @@
             this.gradientLabel1.Controls.Add(this.Bioscoopnaam);
             this.gradientLabel1.EndColor = System.Drawing.Color.Maroon;
             this.gradientLabel1.Location = new System.Drawing.Point(0, 0);
-            this.gradientLabel1.Margin = new System.Windows.Forms.Padding(2);
+            this.gradientLabel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gradientLabel1.Name = "gradientLabel1";
-            this.gradientLabel1.Size = new System.Drawing.Size(1110, 121);
+            this.gradientLabel1.Size = new System.Drawing.Size(1480, 149);
             this.gradientLabel1.TabIndex = 59;
             // 
             // Wittelijn
             // 
             this.Wittelijn.BackColor = System.Drawing.Color.White;
-            this.Wittelijn.Location = new System.Drawing.Point(-2, 116);
+            this.Wittelijn.Location = new System.Drawing.Point(-3, 143);
+            this.Wittelijn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Wittelijn.Name = "Wittelijn";
-            this.Wittelijn.Size = new System.Drawing.Size(1112, 5);
+            this.Wittelijn.Size = new System.Drawing.Size(1483, 6);
             this.Wittelijn.TabIndex = 60;
             // 
             // Bioscoopnaam
@@ -309,18 +332,19 @@
             this.Bioscoopnaam.Cursor = System.Windows.Forms.Cursors.SizeNESW;
             this.Bioscoopnaam.Font = new System.Drawing.Font("Lucida Sans Unicode", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Bioscoopnaam.ForeColor = System.Drawing.Color.Yellow;
-            this.Bioscoopnaam.Location = new System.Drawing.Point(52, 32);
+            this.Bioscoopnaam.Location = new System.Drawing.Point(69, 39);
+            this.Bioscoopnaam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Bioscoopnaam.Name = "Bioscoopnaam";
-            this.Bioscoopnaam.Size = new System.Drawing.Size(289, 59);
+            this.Bioscoopnaam.Size = new System.Drawing.Size(356, 73);
             this.Bioscoopnaam.TabIndex = 10;
             this.Bioscoopnaam.Text = "Darichwiwi";
             // 
             // SeatReservation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1107, 747);
+            this.ClientSize = new System.Drawing.Size(1476, 919);
             this.Controls.Add(this.Screen);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.selectedSeatsPanel);
@@ -343,6 +367,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Linkerkolom);
             this.Controls.Add(this.gradientLabel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "SeatReservation";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SeatReservation";
